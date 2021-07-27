b8ff0a0bb1c809f85a83f760348bc69ff7ac23df
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 23:21:10 +0530

    Uc1 Uploading

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,7 +4,15 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      System.out.println("Welcome to Snake ladder Game");
+      
+      System.out.println("Welcome to Snake and Ladder simulator");
+		int playerPosition = 0;	
+		System.out.println("Starting position is " + playerPosition );
+		
+		int playerPosition1 = 0;
+		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("Dice roll: " + die);
 	}
 
 }
