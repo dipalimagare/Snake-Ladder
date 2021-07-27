57d9e284481963c0d7484ae32028d7cc32ceeda0
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 23:30:32 +0530

    Uc2 Uploading

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
@@ -13,6 +15,30 @@ public class SnakeLadder {
 		int playerPosition1 = 0;
 		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println("Dice roll: " + die);
+		
+		System.out.print("Enter player name: ");
+		Scanner s = new Scanner(System.in);
+		String playerName = s.next();
+
+		System.out.println("Player name: " + playerName);
+		
+		int option = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option) {
+			case 0:
+				System.out.println("No play");
+				playerPosition += 0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				playerPosition += die;
+				break;
+			default:
+				System.out.println("Snake");
+				playerPosition -= die;
+				if (playerPosition < 0)
+					playerPosition = 0;		
+		}
+		System.out.println("Player position: " + playerPosition);
 	}
 
 }
