3c765847b0d896278372d6d485fe0f72b987864c
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-27 23:35:09 +0530

    Uc3 Uploading

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class SnakeLadder {
-
+	public static final int winningPosition = 100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Welcome to Snake ladder Game");
@@ -12,33 +12,27 @@ public class SnakeLadder {
 		int playerPosition = 0;	
 		System.out.println("Starting position is " + playerPosition );
 		
-		int playerPosition1 = 0;
-		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Dice roll: " + die);
-		
-		System.out.print("Enter player name: ");
-		Scanner s = new Scanner(System.in);
-		String playerName = s.next();
-
-		System.out.println("Player name: " + playerName);
-		
-		int option = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (option) {
-			case 0:
-				System.out.println("No play");
-				playerPosition += 0;
-				break;
-			case 1:
-				System.out.println("Ladder");
-				playerPosition += die;
-				break;
-			default:
-				System.out.println("Snake");
-				playerPosition -= die;
-				if (playerPosition < 0)
-					playerPosition = 0;		
-		}
-		System.out.println("Player position: " + playerPosition);
+		while ( winningPosition > playerPosition ) {
+			int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (option) {
+				case 0:
+					System.out.println("No play : " + 0);
+					playerPosition += 0;
+					break;
+				case 1:
+					System.out.println("Ladder  : " + "+" + die);
+					playerPosition += die;
+					break;
+				default:
+					System.out.println("Snake   : " + "-" + die);
+					playerPosition -= die;
+					if (playerPosition < 0)
+						playerPosition = 0;
+			}
+			}
+			System.out.println("Player position: " + playerPosition);
 	}
 
 }
